ff7a7f62ce2c98ba140203ca714aee7b18882264
PhillipBen <null>
2024-07-01 09:49:12 -0500

    Commit Test 1

---
+++ BensonAssignment1Git/src/main/java/com/mycompany/bensonassignment1git/BensonAssignment1Git.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bensonassignment1git;
+
+/**
+ *
+ * @author Phillip
+ */
+public class BensonAssignment1Git {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
