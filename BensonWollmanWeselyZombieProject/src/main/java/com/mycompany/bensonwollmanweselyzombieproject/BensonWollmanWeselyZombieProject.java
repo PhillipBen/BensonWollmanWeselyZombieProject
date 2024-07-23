f5b8d083ed1c14461d835313843e51260bac37d8
PhillipBen <null>
2024-07-23 15:16:25 -0500

    Init Commit

---
+++ BensonWollmanWeselyZombieProject/src/main/java/com/mycompany/bensonwollmanweselyzombieproject/BensonWollmanWeselyZombieProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bensonwollmanweselyzombieproject;
+
+/**
+ *
+ * @author Phillip
+ */
+public class BensonWollmanWeselyZombieProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World2!");
+    }
+}
